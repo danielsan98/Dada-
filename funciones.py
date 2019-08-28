c8123b5104c8aec603adaca4731f8e4d1c87e18c
danielsan98 <38160558+danielsan98@users.noreply.github.com>
2019-08-27 19:25:43 -0500

    Create funciones.py
    funciones de suma, resta, multiplicación y división.

---
+++ funciones.py
@@ -0,0 +1,38 @@
+def suma():
+    num1=int(input("Dame un numero:"))
+    num2=int(input("Dame otro numero:")) 
+
+     resultado=num1+num2
+
+    print("El resultado de la suma es: ", resultado)
+    print("\n")
+
+
+
+def resta():
+    num1=int(input("Dame un numero:"))
+    num2=int(input("Dame otro numero:"))
+
+    resultado=num1-num2
+
+    print("El resultado de la resta es: ", resultado)
+    print("\n")
+
+
+def multiplicacion():
+    num1=int(input("Dame un numero:"))
+    num2=int(input("Dame otro numero:"))
+
+    resultado=num1*num2
+
+    print("El resultado de la multiplicacion es: ", resultado)
+    print("\n")
+
+def division():
+    num1=float(input("Dame un numero:"))
+    num2=float(input("Dame otro numero:"))
+
+    resultado=num1/num2
+
+    print("El resultado de la division es: ", resultado)
+    print("\n")
