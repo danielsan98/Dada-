bd528b4a51a8126b5d6338bb02748d9f4df2c3b4
danielsan98 <38160558+danielsan98@users.noreply.github.com>
2019-09-01 20:23:23 -0500

    tarea

--- chicharronera.py
+++ chicharronera.py
@@ -1,24 +1,26 @@
 import math
 
-def chicharron():
-    a=float(input("dame a"))
-    b=float(input("dame b"))
-    c=float(input("dame c"))
+a=float(input("dame a: "))
+b=float(input("dame b: "))
+c=float(input("dame c: "))
+raiz=0
 
-    if a != 0:
-        negativeB=b*-1
-        d=float((math.pow(b, 2)) - (4*a*c))
-        raiz=float(math.sqrt(d))
-        denominador=2*a
-        if raiz<0:
+if a != 0:
+    negativeB=b*-1
+    d=((math.pow(b, 2)) - (4*a*c))
+    raiz=float(math.sqrt(d))
+    denominador=2*a
+    if raiz<0:
             raiz=raiz *-1
             print("(",negativeB, " +- ", raiz, "i", ")", " / ", 
             denominador)
-        else:
+    else:
             print("(",negativeB, " +- ", raiz, "i", ")", " / ", 
             denominador)
-    else:
-        print('a no puede ser 0!')        
+else:
+        print('a no puede ser 0!')    
+
+
          
 
 
