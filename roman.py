c11bf41f70073f85ea2d0c17279dcd19e3724d0a
danielsan98 <38160558+danielsan98@users.noreply.github.com>
2019-09-19 11:57:55 -0500

    Programa que convierte 1 a 3900 de arabigos a romanos

---
+++ roman.py
@@ -0,0 +1,18 @@
+def convertir():
+    for i in range(1, 3901):
+        Arabigo = int(i)
+        Romano = ""
+        RomanoDict = [[1000, 900, 500, 100, 90, 50, 40, 10, 9, 5, 4, 1], ['M', 'CM', 'D', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']]
+
+        for i in range(len(RomanoDict[0])):
+            if (Arabigo // RomanoDict[0][i] >= 1):
+        
+                Romano += Arabigo // RomanoDict[0][i] * RomanoDict[1][i]
+                Arabigo -= Arabigo // RomanoDict[0][i] * RomanoDict[0][i]
+        print(Romano)  
+
+def main():
+    convertir()
+
+if __name__ == '__main__':
+    main()
