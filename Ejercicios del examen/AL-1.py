82bff57a2826512256395b23a2f425c72e9cb003
danielsan98 <38160558+danielsan98@users.noreply.github.com>
2019-10-06 21:51:27 -0500

    codigos examenes

--- Ejercicios del examen/AL-1.py
+++ Ejercicios del examen/AL-1.py
@@ -1,24 +1,16 @@
-"""
-Pintar los números primos y no primos del 2 al 100
-"""
+def primos(p):
+    primos=[]
+    noPrimos=[]
+    j=0
+    for i in range(2,p):
+        if(i%2==0):
+            primos.append(i)
+        else:
+            noPrimos.append(i)
+    print(primos)
+    print(noPrimos)
 
-def obtenerNumeros(n):
-    primos = [True for i in range (n+1)]
-    
-    for i in range(2,n+1):
-        numDos=2
-        while i*numDos <= n:
-            primos[i*numDos] = False
-            numDos += 1
-    print("Primos: ")
-    for i in range (2,n+1):
-        if primos[i] == True:
-            print(f"{i}, ",end='')
-    print("\n\nNo primos: ")
-    for i in range (2,n+1):
-        if primos[i] == False:
-            print(f"{i}, ",end='')
+        
 
-
-obtenerNumeros(100)
-print("")
+numElement=int(input("¿Hasta que numero quiere que lo numeros primos?"))
+primos(numElement)
